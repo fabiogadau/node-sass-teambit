c463bf892d3405213408cfef8fe42b284703fa9e
Fabio Gadau <fabiogadau92@gmail.com>
2020-05-13 16:03:17 +0200

    first setup

---
+++ dist/scripts/main.js
@@ -0,0 +1,24 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	/************************************************************************/
+(() => {
+/*!*****************************!*\
+  !*** ./src/scripts/main.js ***!
+  \*****************************/
+/*! unknown exports (runtime-defined) */
+/*! exports [maybe provided (runtime-defined)] [unused] */
+/*! runtime requirements:  */
+
+})();
+
+(() => {
+/*!******************************!*\
+  !*** ./src/styles/main.scss ***!
+  \******************************/
+/*! unknown exports (runtime-defined) */
+/*! exports [maybe provided (runtime-defined)] [unused] */
+/*! runtime requirements:  */
+// extracted by mini-css-extract-plugin
+})();
+
+/******/ })()
+;
